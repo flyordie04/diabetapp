2e7441ab7fea80eb7955cd4d51ac6fcc7455c636
Mirek <mirogorzalek@wp.pl>
2018-06-07 23:33:40 +0200

    register + login + addresult

--- app/src/main/java/com/example/mirek/diabetapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mirek/diabetapp/DatabaseHelper.java
@@ -5,29 +5,41 @@ import android.content.Context;
 import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import java.util.Date;
+import android.util.Log;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     private static final String database_name="DiabetApp";
-    //private static final String table_name="Oceny";
+    private static final String table_settings  = "Settings";
+    private static final String table_results  = "Results";
+
+    private static final String CREATE_TABLE_SETTINGS = "CREATE TABLE " + table_settings + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, PHONE STRING)";
+    private static final String CREATE_TABLE_RESULTS = "CREATE TABLE " + table_results + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, RESULT STRING, DATE STRING, TEXT_MESSAGE INT)";
 
     public DatabaseHelper(Context context) {
         super(context, database_name, null, 1);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table Wyniki (ID INTEGER PRIMARY KEY AUTOINCREMENT, RESULT STRING, DATE STRING, TEXT_MESSAGE INT)");
-
+        //db.execSQL("create table " + table_results + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, RESULT STRING, DATE STRING, TEXT_MESSAGE INT)");
+        //db.execSQL("create table Settings (ID INTEGER PRIMARY KEY AUTOINCREMENT, PHONE STRING)");
+        Log.d("Database","2");
+        db.execSQL(CREATE_TABLE_RESULTS);
+        db.execSQL(CREATE_TABLE_SETTINGS);
+        Log.d("Database","3");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS Wyniki");
+        Log.d("Database","4");
+        db.execSQL("DROP TABLE IF EXISTS " + table_results);
+        db.execSQL("DROP TABLE IF EXISTS " + table_settings);
+        Log.d("Database","5");
         onCreate(db);
     }
+
 
     public boolean wstawdane(String result, String date, int text_message){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -35,12 +47,30 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         cv.put("Result", result);
         cv.put("Date", date);
         cv.put("Text_message", text_message);
-        if(db.insert("Wyniki", null, cv)==-1)return false;
+        if(db.insert(table_results, null, cv)==-1)return false;
         else return true;
     }
     public SQLiteCursor pokazdane(){
         SQLiteDatabase db = this.getWritableDatabase();
-        SQLiteCursor cursor = (SQLiteCursor) db.rawQuery("SELECT * FROM Wyniki",null);
+        SQLiteCursor cursor = (SQLiteCursor) db.rawQuery("SELECT * FROM " + table_results, null);
         return cursor;
+    }
+    public SQLiteCursor pokazUstawienia(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteCursor cursor = (SQLiteCursor) db.rawQuery("SELECT * FROM  " + table_settings, null);
+        Log.d("Database","3");
+        return cursor;
+    }
+    public boolean aktualizujUstawienia(String phone){
+        SQLiteDatabase db = this.getWritableDatabase();
+        //SQLiteCursor cursor = (SQLiteCursor) db.rawQuery("SELECT * FROM Settings",null);
+        ContentValues cv = new ContentValues();
+        cv.put("Phone",phone);
+        //if(cursor == null){
+            db.insert(table_settings,null,cv);
+        //} else {
+           //db.update("Settings",cv,"ID = ?", new String[]{"1"});
+        //}
+        return true;
     }
 }
--- app/src/main/java/com/example/mirek/diabetapp/Settings.java
+++ app/src/main/java/com/example/mirek/diabetapp/Settings.java
@@ -1,23 +1,77 @@
 package com.example.mirek.diabetapp;
 
+import android.database.sqlite.SQLiteCursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+    DatabaseHelper db;
+    Button btnSave,btnPokaz;
+    EditText etPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        db = new DatabaseHelper(this);
+        btnSave = findViewById(R.id.btnSave);
+        btnPokaz = findViewById(R.id.btnPokaz);
+
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean powodzenie;
+                powodzenie = db.aktualizujUstawienia(etPhoneNumber.getText().toString());
+                if (powodzenie) {
+                    Toast.makeText(Settings.this, "Powodzenie", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Settings.this, "Nie udało się", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnPokaz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Settings","1");
+                SQLiteCursor kursor = db.pokazUstawienia();
+                Log.d("Settings","2");
+                if (kursor.getCount() > 0) {
+                    Log.d("Settings","3");
+                    StringBuffer buff = new StringBuffer();
+                    while (kursor.moveToNext()) {
+                        buff.append("ID: " + kursor.getString(0) + "\n");
+                        buff.append("Numer: " + kursor.getString(1) + "\n");
+                    }
+                    PokazWiadomosc("Rekord",buff.toString());
+                } else {
+                    PokazWiadomosc("Brak", "Brak rekordów");
+                }
+            }
+        });
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
+    public void PokazWiadomosc(String tytul, String wiadomosc) {
+        AlertDialog.Builder budowniczy = new AlertDialog.Builder(this);
+        budowniczy.setCancelable(true);
+        budowniczy.setMessage(wiadomosc);
+        budowniczy.setTitle(tytul);
+        budowniczy.show();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
